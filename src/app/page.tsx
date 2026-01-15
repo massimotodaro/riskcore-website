48cd7e4c725cc48ec8b2382442a0b3d75169281f
Massimo Todaro <massimotodaro@gmail.com>
2026-01-15 20:13:15 +0000

    Swap DashboardPreview and TrustBadges order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <>
       <Hero />
       <TimeTravelSection />
-      <TrustBadges />
       <DashboardPreview />
+      <TrustBadges />
       <Features />
       <CTASection />
     </>
